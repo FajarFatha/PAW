d9493f5fd2a5f53f990b9851faeadd473b13a854
FajarFatha <fajarfathar@gmail.com>
2022-03-08 22:55:53 +0700

    membuat file fungsi.php

---
+++ Tugas 2/fungsi.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    function mybio($nama, $age){
+        echo "namaku $nama, Saya berumur $age tahun";
+        echo "<br>";
+    }
+
+    mybio("fajar", "18");
+    mybio("fatha", "19");
+    mybio("roma", "20");
+    ?>
+</body>
+</html>
