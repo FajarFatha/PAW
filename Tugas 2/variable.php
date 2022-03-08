cd88a29e5ca864f4760f25139227682a7d17de14
FajarFatha <fajarfathar@gmail.com>
2022-03-08 22:32:09 +0700

    membuat file variable.php

---
+++ Tugas 2/variable.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>    
+<?php
+    $angka1 = 4;
+    $angka2 = 8;
+    $angka3 = 10;
+
+    $hasil_jumlah = $angka1 + $angka2;
+    $hasil_kurang = $angka3 - $angka1;
+    $hasil_kali = $angka2 * $angka3;
+
+    echo "angka 1 = $angka1";
+    echo "<br>";
+    echo "angka 2 = $angka2";
+    echo "<br>";
+    echo "angka 3 = $angka3";
+    echo "<br>";
+
+
+    echo "$angka1+$angka2 = $hasil_jumlah";
+    echo "<br>";
+    echo "$angka3-$angka1 = $hasil_kurang";
+    echo "<br>";
+    echo "$angka2*$angka3 = $hasil_kali";
+    echo "<br>";
+
+
+?>
+</body>
+</html>
